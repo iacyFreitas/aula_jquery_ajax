7352d53249d4b51cabb34cfbe9bf00f3609b45f4
pitico12 <iacyfreitasbr@gmail.com>
2021-11-14 09:01:46 -0300

    aula jquery_ajax

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function ConsultaCep(){
+    var cep=document.getElementById('cep').value;
+    var url= "https://viacep.com.br/ws/"+cep+"/json"
+    
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $('#logradouro').html(response.logradouro);
+            $('#bairro').html(response.bairro);
+            $('#localidade').html(response.localidade);
+            $('#uf').html(response.uf);           
+        }
+    })
+}
